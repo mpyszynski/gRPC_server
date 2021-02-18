ccb00f943b49d6bb0a7007fa9f192b9f6d485e93
Michal Pyszynski <michal.pyszynski@coop.no>
2021-02-18 10:05:06 +0100

    Update generate_protos

--- generate_protos.sh
+++ generate_protos.sh
@@ -5,6 +5,6 @@ rm proto/echo-service_grpc.pb.go || true
 rm proto/echo-service.pb.go || true
 
 # Generate protobuf and grpc service
-protoc --go_out=.  --go-grpc_out=. --go_opt=paths=source_relative  --go-grpc_opt=paths=source_relative proto/echo-service.proto
-
+protoc --go_out=. --go_opt=paths=source_relative proto/echo-service.proto
+protoc --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/echo-service.proto
 
